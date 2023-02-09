7159abd11b1196b4be826e837d73c141817a35c8
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-02-09 09:35:03 +0500

    feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Feed.css";
 import TweetBox from "./TweetBox";
+import Post from "./Post";
 
 function Feed() {
     return (
@@ -11,11 +12,11 @@ function Feed() {
 
             <TweetBox/>
 
-            {/* Post */}
-            {/* Post */}
-            {/* Post */}
-            {/* Post */}
-            {/* Post */}
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
         </div>
     );
 }
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -7,10 +7,12 @@ function TweetBox() {
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
-                    <Avatar src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2FBek_Mirahmedov%2Fstatus%2F1485375543809826823&psig=AOvVaw1KKFMfj7xoFSRqCJYTM9C1&ust=1675959145403000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNClh76ohv0CFQAAAAAdAAAAABAE"></Avatar>
+                    <Avatar src="https://media.licdn.com/dms/image/C4D0BAQHzK9-4vKacXQ/company-logo_200_200/0/1673250788390?e=2147483647&v=beta&t=gTBVekFc9uQ-qdOQcpnT75_K63SFNNxQ8T8RC_Kr3DM"></Avatar>
                     <input placeholder="What's happening?" type="text"/>
                 </div>
-                <input className="tweetBox__imageInput" placeholder="Enter image URL" type="text"/>
+                <input className="tweetBox__imageInput"
+                       placeholder="Optional: Enter image URL"
+                       type="text"/>
                 <Button className="tweetBox__tweetButton">Tweet</Button>
             </form>
         </div>
